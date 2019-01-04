24731d5d66394b9a5ff693607c429eda5b2641b4
lkd <andy4sake@outlook.com>
2019-01-04 18:11:07 +0800

    api

--- app.js
+++ app.js
@@ -130,6 +130,6 @@ App({
   // apiUrl:"http://192.168.0.199:8081"
   //  apiUrl: "https://api.jcrsjy.com"
   // apiUrl: "https://api.qinzirongyao.com"
-  // apiUrl: "https://api.sahuanka.com" //正式
-  apiUrl: "https://test.jcrsjy.com" //测试
+  apiUrl: "https://api.sahuanka.com" //正式
+  // apiUrl: "https://test.jcrsjy.com" //测试
 })
